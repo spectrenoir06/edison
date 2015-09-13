0e7e1aeb7b06c1485c962e21018c25b00d8425c6
Lucile <LLucile@users.noreply.github.com>
2015-09-13 11:33:56 +0200

    Creation of globals.py

---
+++ globals.py
@@ -0,0 +1,7 @@
+def enum(**named_values):
+    return type('Enum', (), named_values)
+
+Alert = enum(TEMP_LOW='temperature low', TEMP_HIGH='temperature high', ACTIVE='active', BARKING='barking')
+
+buffer = ""
+
