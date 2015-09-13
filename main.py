bc676f24ed7195c9b54f89b6fed1a0d3ffafe662
Lucile <LLucile@users.noreply.github.com>
2015-09-13 11:35:43 +0200

    Added references to outer functions

--- main.py
+++ main.py
@@ -2,59 +2,41 @@ import socket
 import os
 import sys
 from time import sleep
+from connexionManager import *
+from tempProc import *
+from soundProc import *
+from accProc import *
+from globals import *
 import mraa
-import pyupm_mic as upmMicrophone 
+import pyupm_mic as upmMicrophone
 import pyupm_mma7660 as upmMMA7660
 
-myDigitalAccelerometer = upmMMA7660.MMA7660(upmMMA7660.MMA7660_I2C_BUS, upmMMA7660.MMA7660_DEFAULT_I2C_ADDR)
-
-myDigitalAccelerometer.setModeStandby()
-myDigitalAccelerometer.setSampleRate(upmMMA7660.MMA7660.AUTOSLEEP_64)
-myDigitalAccelerometer.setModeActive()
-
-TCP_IP = '192.168.100.62'
-TCP_PORT = 1234
-BUFFER_SIZE = 1024
+# Init accelerometer
+initAcc()
+# Init messages list
+message = []
 
 while (1):
+    global message
+    global buffer
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    while (1):
-        try:
-            s.connect((TCP_IP, TCP_PORT))
-            break
-        except:
-            print("erreur fdp")
-            sleep(1)
-
-
+    connexionInit(s)
     s.send("login:\n")
-    
-    temperature = mraa.Aio(0)
-    myMic = upmMicrophone.Microphone(1)
-    
-    threshContext = upmMicrophone.thresholdContext()
-    threshContext.averageReading = 0
-    threshContext.runningAverage = 0
-    threshContext.averagedOver = 2
-	
+    initTemp()
+    initSound()
+    initAcc()
+    print("test")
 
-    print("test")   
- 
-    x = upmMMA7660.new_intp()
-    y = upmMMA7660.new_intp()
-    z = upmMMA7660.new_intp()
+    i = 0
 
-    i = 0;
     while (1):
-       buffer = upmMicrophone.uint16Array(32)
-       len = myMic.getSampledWindow(2, 32, buffer)        
-       if len:
-          thresh = myMic.findThreshold(threshContext, 30, buffer, len)
+        buffer = ""
+        alert = alertTemp(message) + alertSound(message) + alertProc(message)
+        buffer += "\n"
 
-        
-       myDigitalAccelerometer.getRawValues(x, y, z)
-       
-       s.send(str(temperature.read()) + ";" + str(thresh) + ", x=" + str(upmMMA7660.intp_value(x)) + ", y=" + str(upmMMA7660.intp_value(y)) + ", z=" + str(upmMMA7660.intp_value(z)) + "\n")
-       i = i + 1 
-       sleep(i)
+
+
+        s.send(buffer)
+        s.send( )
+        sleep(i)
     s.close()
