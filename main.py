2239cc23f67b87f2f85232d914aefb549840f90b
Lucile <LLucile@users.noreply.github.com>
2015-09-13 13:07:37 +0200

    Added personnalised delay management

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import socket
 import os
 import sys
-from time import sleep
+from time import *
 from connexionManager import *
 from tempProc import *
 from soundProc import *
@@ -15,7 +15,15 @@ import pyupm_mma7660 as upmMMA7660
 # Init accelerometer
 initAcc()
 
+var = input("Please enter the update delay in seconds : ")
+delay = float(var)
+print("you entered : " + str(delay) + "s \n")
+
+t = time.time()
+
+buffer_str = ""
 while (1):
+    dt = time.time()- t
     global message
     global buffer
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -33,8 +41,10 @@ while (1):
         buffer = []
         message = []
         alert = alertTemp(message, buffer) + alertSound(message, buffer) + alertAcc(message, buffer)
-        buffer_str = buffer[0] + buffer[1] + buffer[2] + "\n"
-        print(buffer_str)
-        s.send(buffer_str)
-        sleep(i)
+        buffer_str += buffer[0] + buffer[1] + buffer[2] + "\n"
+        if dt > delay or alert:
+            print(buffer_str)
+            s.send(buffer_str)
+            t = time.time()
+            buffer_str = ""
     s.close()
